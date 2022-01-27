9850f4ec93d7d5e43ff68735022c6260ffdb76a5
Min kyeong Tak <mtak0235@gmail.com>
2022-01-27 17:00:09 +0900

    map ing

--- map.hpp
+++ map.hpp
@@ -1,8 +1,20 @@
 #ifndef MAP_HPP
 #define MAP_HPP
 
+#include <memory>
+#include "ft_utility.hpp"
+
 namespace ft {
 	
+	template <class Key, class T, class Compare = less<Key>, class Alloc = std::allocator<pair<const Key, T>>>
+	class map {
+		typedef pair<const Key, T>			valuetype;
+		typedef Key							key_type;
+		typedef T							mapped_type;
+		typedef 
+
+	};
+
 }
 
 #endif
